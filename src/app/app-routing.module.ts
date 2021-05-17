ff3cfc0e78a80f43da40ec53a71f3728a65f530b
laukik-code <laukik@codixlabs.com>
2021-05-17 14:58:55 +0530

    base angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { CreateComponent } from './create/create.component';
+import { UpdateComponent } from './update/update.component';
+
+const routes: Routes =
+  [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'details/:productId', component: DetailsComponent },
+    { path: 'create', component: CreateComponent },
+    { path: 'update/:productId', component: UpdateComponent }
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { CreateComponent } from './create/create.component';
+import { UpdateComponent } from './update/update.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    CreateComponent,
+    UpdateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
